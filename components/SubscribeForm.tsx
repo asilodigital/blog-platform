5b3f50f4920686b7c305994854728729200d095e
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2023-05-03 13:57:31 -0400

    Applied dimension styles to SubscribeForm elements

--- components/SubscribeForm.tsx
+++ components/SubscribeForm.tsx
@@ -11,7 +11,7 @@ export const SubscribeForm = ({ settings }: { settings: GhostSettings }) => {
   return (
     <form className={message} data-members-form="subscribe" onSubmit={(ev) => handleSubmit(ev, settings.url)}>
       <div className="form-group">
-        <label htmlFor="email">
+        {/* <label htmlFor="email">
           {text(`EMAIL`)}
           <style jsx>{`
             label {
@@ -21,7 +21,7 @@ export const SubscribeForm = ({ settings }: { settings: GhostSettings }) => {
               overflow: hidden;
             }
           `}</style>
-        </label>
+        </label> */}
         <input
           id="email"
           name="email"
