a5b8dd18c61962b725c622b769ddfa586e6d84d4
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2023-05-03 13:37:45 -0400

    Cambiar copies integrados en componente de tarjeta de subscripción

--- components/SubscribeForm.tsx
+++ components/SubscribeForm.tsx
@@ -34,7 +34,7 @@ export const SubscribeForm = ({ settings }: { settings: GhostSettings }) => {
           autoComplete="false"
         />
         <button className="button primary" type="submit" value="Submit">
-          <span className="button-content">{text(`SUBSCRIBE`)}</span>
+          <span className="button-content">{text(`SUBSCRIBE_ME`)}</span>
           <span className="button-loader">
             <LoaderIcon />
           </span>
